6a8804e001845d0c2e771c03455c6d6870560fe2
Steph <zhayixin2004@gmail.com>
2024-10-30 00:33:25 -0400

    modified yolov5.py

--- src/yolov5.py
+++ src/yolov5.py
@@ -12,7 +12,7 @@ import cv2
 
 # get model with transfer learning
 # cited from here: https://universe.roboflow.com/workspace01-ae0oa/fridgify
-rf = Roboflow(api_key="ayL48plh8fqJBfMTdBUb")
+rf = Roboflow(api_key="CHANGE TO YOUR OWN API KEY")
 project = rf.workspace().project("fridgify")
 model = project.version(3).model
 
