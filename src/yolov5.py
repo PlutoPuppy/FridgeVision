b4b5f7c94dccba9f7b95b0a9da812f23d525fc4f
Steph <zhayixin2004@gmail.com>
2024-10-30 00:16:25 -0400

    added some data and yolov5.py

---
+++ src/yolov5.py
@@ -0,0 +1,29 @@
+
+# do: "pip install roboflow supervision opencv-python"
+# to get packages needed
+# or use "pip3"
+
+# importing libraries
+import torch
+from roboflow import Roboflow
+import supervision as sv
+import cv2
+
+
+# get model with transfer learning
+# cited from here: https://universe.roboflow.com/workspace01-ae0oa/fridgify
+rf = Roboflow(api_key="ayL48plh8fqJBfMTdBUb")
+project = rf.workspace().project("fridgify")
+model = project.version(3).model
+
+
+# feed with our own data
+result = model.predict("FridgeVision/sample-images/foodsInFridge.jpg", confidence=40, overlap=30).json()
+labels = [item["class"] for item in result["predictions"]]
+print(result)
+print(labels)
+
+
+
+
+
